1af8fdcf51c628ed0fba1939d7619dba5ea93c03
Amr Waheed <amrmohamed877@gmail.com>
2024-09-06 22:40:13 +0300

    new robofriends project

---
+++ src/robots.js
@@ -0,0 +1,63 @@
+const robots = [
+    {
+        id: 1,
+        Name: "Amr Waheed",
+        username: "amr",
+        email: "amr@gmail.com",
+    },
+    {
+        id: 2,
+        Name: "Mohamed Waheed",
+        username: "mohamed",
+        email: "mohamed@gmail.com",
+    },
+    {
+        id: 3,
+        Name: "Ahmed Waheed",
+        username: "ahmed",
+        email: "ahmed@gmail.com",
+    },
+    {
+        id: 4,
+        Name: "Amira Waheed",
+        username: "amira",
+        email: "amira@gmail.com",
+    },
+    {
+        id: 5,
+        Name: "Basma Waheed",
+        username: "Basma",
+        email: "Basma@gmail.com",
+    },
+    {
+        id: 6,
+        Name: "Malak Amr",
+        username: "malak",
+        email: "malak@gmail.com",
+    },
+    {
+        id: 7,
+        Name: "Youssef Amr",
+        username: "youssef",
+        email: "youssef@gmail.com",
+    },
+    {
+        id: 8,
+        Name: "Mariam Amr",
+        username: "mariam",
+        email: "mariam@gmail.com",
+    },
+    {
+        id: 9,
+        Name: "Maria Amr",
+        username: "maria",
+        email: "maria@gmail.com",
+    },
+    {
+        id: 10,
+        Name: "Amr Waheed",
+        username: "amr",
+        email: "amr@gmail.com",
+    }
+]
+export default robots
